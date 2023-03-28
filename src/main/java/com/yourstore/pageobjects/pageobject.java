6b8e1cbd769b83697e8defc8253fda0c0e4f4429
Kondapalli Sai Krishna <kondapalli.krishna@telusinternational.com>
2023-03-28 11:14:25 +0530

    first commit

---
+++ src/main/java/com/yourstore/pageobjects/pageobject.java
@@ -0,0 +1,10 @@
+package com.yourstore.pageobjects;
+
+import com.yourstore.base.Baseclass;
+
+public class pageobject extends Baseclass{
+	
+	public void m1() {
+		System.out.println("the driver has been run");
+	}
+}
---
+++ src/main/java/com/yourstore/utilites/Utilites.java
@@ -0,0 +1,26 @@
+package com.yourstore.utilites;
+
+
+
+
+
+import java.util.Date;
+
+import com.yourstore.base.Baseclass;
+
+public class Utilites extends Baseclass {
+
+	public static final int IMPLICIT_WAIT_TIME = 10;
+	
+	public static final long PAGE_Load_TIME = 0;
+	
+
+	public static String random_EmailID() {
+		Date date = new Date();
+		String num = date.toString().replace(" ", "_").replace(":", "_");
+		String email = "sai" + num + "@gmail.com";
+		return email;
+
+	}
+}
+
